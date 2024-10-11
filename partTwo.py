0462bf48ac43a8175fbf5aaa98665544bd5bf23f
ameliechamp <ac2242@kent.ac.uk>
2024-10-11 11:12:41 +0000

    kaka

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,10 @@
+import random
+secret_number = random.randint(1,10)
+guess = int(input("What is my number?: "))
 
+if guess > secret_number:
+    print("Too high.")
+elif guess < secret_number:
+    print("Too low.")
+else:
+    print("Correct!")
