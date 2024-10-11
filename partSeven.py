041b7c677c3c9427cd7c85c4d783b24b97756c8c
ameliechamp <ac2242@kent.ac.uk>
2024-10-11 12:22:42 +0000

    oo

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,27 @@
+num1 = int(input("Enter number one:"))
+num2 = int(input("Enter number two:"))
+operator = input("Enter an operator (+,-,*,/,^,%): ")
 
+match operator:
+    case "+":
+        add = num1+num2
+        print(add)
+    case "-":
+        sub = num1-num2
+        print(sub)
+    case "+":
+        mult = num1*num2
+        print(mult)
+    case "/":
+        div = num1/num2
+        print(div)
+    case "^":
+        power = num1^num2
+        print(power)
+    case "%":
+        mod = num1%num2
+        print(mod)
+    case _:
+        print("Quitting the program...")
+            
+        
--- partSix.py
+++ partSix.py
@@ -1,1 +1,10 @@
-
+age = int(input("Enter in your age: "))
+student = input("Are you a student?: ").lower()
+if age < 12 or age >= 65:
+    print("Your ticket costs £5.")
+elif 12<=age<=64 and student == "no":
+    print("Your ticket costs £10")
+elif 12<=age<=64 and student == "yes":
+    print("Your ticket costs £8")
+else:
+    print("Not a valid input.")
