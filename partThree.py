29fa81b95a87994ee0dfb30cd45dc7624fd648d1
ameliechamp <ac2242@kent.ac.uk>
2024-10-11 11:24:00 +0000

    77

--- partThree.py
+++ partThree.py
@@ -1,1 +1,13 @@
-
+score = int(input("What is your score?: "))
+if 90>=score<=100:
+    print("A")
+elif 89>=score<=80:
+    print("B")
+elif 79>=score<=70:
+    print("C")
+elif 69>=score<=60:
+    print("D")
+elif 60>=score:
+    print("F")
+else:
+    print("That is not a valid input.")
